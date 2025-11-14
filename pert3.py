30b29a98c238bfb43e9d10e9ee2fb798e4178054
baiqputribungaaulia <baiqputribungaaulia@gmail.com>
2025-11-14 18:25:14 +0800

    Create pert3.py

---
+++ pert3.py
@@ -0,0 +1,17 @@
+# kita akan belajar tipe data (jenis2 data)
+
+x = 10  # bilangan bulat (integer "int")
+print("nilai dari x :", x)
+print(type(x))
+
+y = 3.5  # bilangan pecahan (float "float")
+print("nilai dari y :", y)
+print(type(y))
+
+nama = "baiq bunga"  # tipe data string (tulisan "str")
+print("nilai dari nama :", nama)
+print(type(nama))
+
+kebenaran = False  # tipe data benar salah (True / False "bool")
+print("nilai dari kebenaran :", kebenaran)
+print(type(kebenaran))
