bde02d86557be13524fc124c273302331d677666
baiqputribungaaulia <baiqputribungaaulia@gmail.com>
2025-11-14 18:33:58 +0800

    Add user input and type display in pert4.py

---
+++ pert4.py
@@ -0,0 +1,13 @@
+# mengambil inputan dari user
+
+nama = input("masukan nama anda :")
+print("selamat datang ", nama)
+print(type(nama))
+
+umur = int(input("masukan umur anda :"))
+print("umur anda sekarang ", umur)
+print(type(umur))
+
+kejujuran = bool(input("apakah kamu selingkuh? :"))
+print("ooo kejujuranmu ", kejujuran)
+print(type(kejujuran))
