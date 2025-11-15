0483884ec2e6a911918144f5d4e7657f2fbae53a
baiqputribungaaulia <baiqputribungaaulia@gmail.com>
2025-11-15 16:16:11 +0800

    Add user input for name and NIM
    Added input prompts for name and NIM, and printed their values and types.

--- pert4.py
+++ pert4.py
@@ -11,3 +11,16 @@ print(type(umur))
 kejujuran = bool(input("apakah kamu selingkuh? :"))
 print("ooo kejujuranmu ", kejujuran)
 print(type(kejujuran))
+
+
+
+
+
+nama = input("Masukkan nama anda : ")
+nim = int(input("Masukkan NIM anda : "))
+
+print("Nama anda adalah", nama)
+print("NIM anda adalah", nim)
+
+print(type(nama))
+print(type(nim))
